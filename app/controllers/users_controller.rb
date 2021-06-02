e2f2b902382965402792e7aa2c930d7075861dad
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-06-02 20:05:06 +0000

    add action methods for users_controllers and allow users to view signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,26 @@ class UsersController < ApplicationController
     erb :login
   end
 
+  post '/login' do
+    @user = User.find_by(username: params[:username])
+
+    if @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect to "/users/#{@user.id}"
+    else
+      erb :signup
+    end
+  end
+
   get '/signup' do
     erb :signup
+  end
+
+  post '/signup' do
+    @user = User.create(params)
+  end
+
+  get '/users/:id' do
+    "this will be the showpage"
   end
 end
